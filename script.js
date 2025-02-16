ac7764d12a69806ba22b5d911dd63cc79c9362d5
kulikovavarvara <151393496+kulikovavarvara@users.noreply.github.com>
2025-02-16 10:24:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+class student {
+    constructor(nameStudent,ageStudent,facultyStudent,gradeStudent){
+        this.name = nameStudent;
+        this.age = ageStudent;
+        this.faculty = facultyStudent;
+        this.grade = gradeStudent;
+    }
+
+    getInfo(){
+        console.log(`привет, я ${this.name} я студент ${}`)
+    }
+
+    changeGrade
+    console.log()
+
+    class ElectronicDelice{
+constructor
+    }
+
+}
